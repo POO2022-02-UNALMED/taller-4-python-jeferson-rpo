4bff58802b0d2429e529acfdcb1755e1104c1740
jeferson-rpo <98125058+jeferson-rpo@users.noreply.github.com>
2022-09-04 22:13:37 -0500

    editar

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,5 +1,9 @@
+
 class Asignatura:
 
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
+    def __str__(self):
+      hay="Matematicas remoto"
+      return hay
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -5,33 +5,44 @@ from classroom.asignatura import Asignatura
 
 
 class Grupo:
-
+    
     grado = None
-
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo ordinado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
+        self.grado= "grado 12 "
+        
 
-    def listadoAsignaturas(self, kwargs):
+
+
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
-
+    
     def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
+       
+        if (lista is None):
             lista.append(alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
+            
         else:
-            self.listadoAlumnos = [alumno]
+           self.listadoAlumnos = [alumno]
+           self.listadoAlumnos = self.listadoAlumnos + lista
+          
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+    def __str__(self):
+      hay="Grupo de estudiantes: grupo predeterminado "
+      return hay
+
+  #  @ classmethod
+   # def asignarNombre(cls, nombre="Grado 12"):
+     #   cls.grado = nombre
+
+  #  @ classmethod
+    #def asignarNombre(cls, nombre="Grado 6"):
+     #   cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
